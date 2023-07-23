7d34b473a91e5fcda5b9b1fe53239edd47357b5c
dzering <71943051+dzering@users.noreply.github.com>
2023-07-23 14:54:48 +0300

    The player model was added

---
+++ Assets/CursedCastle/Animations/TestAnim.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CursedCastle.CodeBase.Infrastructure;
+using CursedCastle.InputSystem;
+using Unity.Collections;
+using UnityEngine;
+
+public class TestAnim : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private InputProvider inputProvider;
+    private bool isActive;
+
+    private void Start()
+    {
+        inputProvider = AllServices.Container.Single<IInputService>().InputProvider;
+        inputProvider.OnUseAction += ButtonAction;
+        isActive = false;
+    }
+
+    private void ButtonAction()
+    {
+        SwitchAnimation(isActive);
+        isActive = !isActive;
+    }
+
+    private void SwitchAnimation(bool isActive)
+    {
+        _animator.SetBool("Color", isActive);
+        _animator.SetBool("Vertical", !isActive);
+    }
+}
--- Assets/CursedCastle/CodeBase/Character/Selector/ISwitchable.cs
+++
@@ -1,8 +0,0 @@
-namespace CursedCastle.CodeBase.Character.Selector
-{
-    public interface ISwitchable
-    {
-        void SwitchOn();
-        void SwitchOff();
-    }
-}
